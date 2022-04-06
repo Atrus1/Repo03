1f7c772f94a95ed0115aa483b5bd08257b8c1704
Atrus1 <atrus2012@gmail.com>
2022-04-06 12:53:06 +0200

    Adding files

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,15 +1,8 @@
 function main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
+//
+console.log('Hello World!');
+//
 
-    console.log('Hello World!');
-    
-=======
-    console.log('Hello World!');
->>>>>>> 703c7e740397009e86460f7ae072320abf3b300f
-=======
-    console.log('Hello World!');
->>>>>>> 703c7e740397009e86460f7ae072320abf3b300f
     }
     main();
 module.exports = main;
